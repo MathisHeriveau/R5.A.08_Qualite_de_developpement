e6a5d6bdd67e4da29d850d0ba12aaa6927ee26f4
Mathis heriveau <mheriveau@iutbayonne.univ-pau.fr>
2023-09-26 10:54:56 +0200

    Eyo, je push

--- src/main/java/com.heriveau.calculator/Calculator.java
+++ src/main/java/com.heriveau.calculator/Calculator.java
@@ -1,4 +1,51 @@
 package com.heriveau.calculator;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Calculator {
+
+    public int add(int opG, int opD) {
+        if ((opG > 0 && opD > Integer.MAX_VALUE - opG) || (opG < 0 && opD < Integer.MIN_VALUE - opG)) {
+            // Gérer le dépassement de la plage autorisée pour un entier
+            System.err.println("Dépassement de la plage autorisée pour un entier lors de l'addition.");
+            return 0; // Ou tout autre traitement d'erreur approprié
+        } else {
+            // Effectuer l'addition en toute sécurité
+            return opG + opD;
+        }
+    }
+
+    public int divise(int opG, int opD) {
+        if (opD == 0) {
+            // Gérer la division par zéro
+            System.err.println("Division par zéro détectée.");
+            return 0; // Ou tout autre traitement d'erreur approprié
+        } else {
+            // Effectuer la division en toute sécurité
+            return opG / opD;
+        }
+    }
+
+    public Set<Integer> ensembleChiffres(int pNombre) {
+        Set<Integer> chiffres = new HashSet<>();
+
+        // Convertir le nombre en positif si nécessaire
+        int nombre = Math.abs(pNombre);
+
+        // Traitement spécial pour 0
+        if (nombre == 0) {
+            chiffres.add(0);
+            return chiffres;
+        }
+
+        while (nombre > 0) {
+            int chiffre = nombre % 10;
+            chiffres.add(chiffre);
+            nombre /= 10;
+        }
+
+        return chiffres;
+    }
+
 }
--- src/test/java/com/heriveau/calculator/CalculatorTest.java
+++ src/test/java/com/heriveau/calculator/CalculatorTest.java
@@ -1,5 +1,121 @@
 package com.heriveau.calculator;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CalculatorTest {
-    
+
+    Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        /* On construit un objet Calculator */
+        calculator = new Calculator();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1, 1",
+            "1, 2, 3",
+            "-2, 2, 0",
+            "0, 0, 0",
+            "-1, -2, -3"
+    })
+    void verification_fonction_addition(int opG, int opD, int resultatAttendu) {
+        int resultat = calculator.add(opG, opD);
+        assertEquals(resultatAttendu, resultat);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1, 0",
+            "1, 2, 0",
+            "-2, 2, -1",
+            "0, 0, 0",
+            "-1, -2, 0"
+    })
+    void verification_fonction_division(int opG, int opD, int resultatAttendu) {
+        int resultat = calculator.divise(opG, opD);
+        assertEquals(resultatAttendu, resultat);
+    }
+
+    @Test
+    void testEnsembleChiffresPositif() {
+        int nombre = 7679;
+        Set<Integer> chiffres = calculator.ensembleChiffres(nombre);
+
+        assertTrue(chiffres.contains(6));
+        assertTrue(chiffres.contains(7));
+        assertTrue(chiffres.contains(9));
+        assertEquals(3, chiffres.size());
+    }
+
+    @Test
+    void testEnsembleChiffresNegatif() {
+        int nombre = -11;
+        Set<Integer> chiffres = calculator.ensembleChiffres(nombre);
+
+        assertTrue(chiffres.contains(1));
+        assertEquals(1, chiffres.size());
+    }
+
+    @Test
+    void testEnsembleChiffresZero() {
+        int nombre = 0;
+        Set<Integer> chiffres = calculator.ensembleChiffres(nombre);
+
+        assertTrue(chiffres.contains(0));
+        assertEquals(1, chiffres.size());
+    }
+
+    @Test
+    void testEnsembleChiffresNegatifZero() {
+        int nombre = -0;
+        Set<Integer> chiffres = calculator.ensembleChiffres(nombre);
+
+        assertTrue(chiffres.contains(0));
+        assertEquals(1, chiffres.size());
+    }
+
+    @Test
+    void testEnsembleChiffresGrandNombre() {
+        int nombre = 1234567890;
+        Set<Integer> chiffres = calculator.ensembleChiffres(nombre);
+
+        assertTrue(chiffres.contains(0));
+        assertTrue(chiffres.contains(1));
+        assertTrue(chiffres.contains(2));
+        assertTrue(chiffres.contains(3));
+        assertTrue(chiffres.contains(4));
+        assertTrue(chiffres.contains(5));
+        assertTrue(chiffres.contains(6));
+        assertTrue(chiffres.contains(7));
+        assertTrue(chiffres.contains(8));
+        assertTrue(chiffres.contains(9));
+        assertEquals(10, chiffres.size());
+    }
+
+    @Test
+    void testEnsembleChiffresZeroMultiple() {
+        int nombre = 10000;
+        Set<Integer> chiffres = calculator.ensembleChiffres(nombre);
+
+        assertTrue(chiffres.contains(0));
+        assertTrue(chiffres.contains(1));
+        assertEquals(2, chiffres.size());
+    }
+
+    @AfterEach
+    void tearDown() {
+        /* On détruit l'objet Calculator */
+        calculator = null;
+    }
 }
